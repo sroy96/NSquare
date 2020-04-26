c4c7991605ee87f41040fa7968b5fda4f23d03d3
saurav-roy <saurav_roy@apple.com>
2020-04-26 18:01:37 +0530

    Repo Setup

---
+++ Backend/src/main/java/tech/NSquare/N2/N2Application.java
@@ -0,0 +1,13 @@
+package tech.NSquare.N2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N2Application.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/tech/NSquare/N2/N2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.NSquare.N2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
